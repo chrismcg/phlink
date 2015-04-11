cd7017be1e4cbc98ccf3a2efc51d4e266daf898c
Chris McGrath <chris@chrismcg.com>
2015-04-11 21:14:22 +0100

    Remove generated page files

--- test/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule Phlink.PageControllerTest do
-  use Phlink.ConnCase
-
-  test "GET /" do
-    conn = get conn(), "/"
-    assert conn.resp_body =~ "Welcome to Phoenix!"
-  end
-end
--- web/views/page_view.ex
+++
@@ -1,3 +0,0 @@
-defmodule Phlink.PageView do
-  use Phlink.Web, :view
-end
