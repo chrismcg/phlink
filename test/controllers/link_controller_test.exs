f9487d086e6cedc4374f18c56cc059c3763d7b86
Chris McGrath <chris@chrismcg.com>
2015-04-17 08:21:29 +0100

    Tidy up tests a little

--- test/controllers/link_controller_test.exs
+++ test/controllers/link_controller_test.exs
@@ -1,16 +1,17 @@
 defmodule Phlink.LinkControllerTest do
   use Phlink.ConnCase
 
+  @model %Link{url: "http://example.com", shortcode: "abc"}
+
   test "GET / renders new link form" do
     conn = get conn(), "/"
     assert conn.resp_body =~ ~r/<input.*?name="link\[url\]"/
   end
 
   test "POST /shorten creates a shortened url and redirects" do
-    url = "http://example.com"
-    expected_shortcode = UUID.uuid5(:url, url, :hex)
+    expected_shortcode = UUID.uuid5(:url, @model.url, :hex)
     assert link_count == 0
-    conn = post conn(), "/shorten", %{"link": %{"url": url}}
+    conn = post conn(), "/shorten", %{"link": %{"url": @model.url}}
     assert link_count == 1
     link = Repo.one!(from l in Link, select: l)
     assert link.shortcode == expected_shortcode
@@ -19,32 +20,30 @@ defmodule Phlink.LinkControllerTest do
   end
 
   test "POST /shorten errors if the url is blank" do
-    url = ""
     assert link_count == 0
-    conn = post conn(), "/shorten", %{"link": %{"url": url}}
+    conn = post conn(), "/shorten", %{"link": %{"url": ""}}
     assert link_count == 0
     assert conn.status == 200
     assert conn.resp_body =~ "Url can&#39;t be blank"
   end
 
   test "POST /shorten errors if the url isn't a valid url" do
-    url = "not a url"
     assert link_count == 0
-    conn = post conn(), "/shorten", %{"link": %{"url": url}}
+    conn = post conn(), "/shorten", %{"link": %{"url": "not a url"}}
     assert link_count == 0
     assert conn.status == 200
     assert conn.resp_body =~ "Url is invalid"
   end
 
   test "GET /shorten/:id displays link and short link" do
-    link = Repo.insert(%Link{url: "http://example.com", shortcode: "abc"})
+    link = Repo.insert(@model)
     conn = get conn(), "/shorten/#{link.id}"
     assert conn.resp_body =~ ~r{<a.*?href="http://example.com"}
     assert conn.resp_body =~ ~r{<a.*?href="http://localhost:4001/abc"}
   end
 
   test "GET /:shortcode redirects to url matching shortcode" do
-    link = Repo.insert(%Link{url: "http://example.com", shortcode: "abc"})
+    Repo.insert(@model)
     conn = get conn(), "/abc"
     assert conn.status == 301
     assert Enum.any?(conn.resp_headers, &(&1 == {"Location", "http://example.com"}))
