fda5c05ff11a80732f48aa6a888808c8f6d6c6cf
Chris McGrath <chris@chrismcg.com>
2015-04-19 21:20:36 +0100

    Unshorten doesn't need full browser pipeline

--- web/router.ex
+++ web/router.ex
@@ -8,12 +8,20 @@ defmodule Phlink.Router do
     plug :protect_from_forgery
   end
 
+  pipeline :unshorten do
+    plug :accepts, ["html"]
+  end
+
+  scope "/", Phlink do
+    pipe_through :unshorten
+    get "/:shortcode", LinkController, :unshorten
+  end
+
   scope "/", Phlink do
     pipe_through :browser # Use the default browser stack
 
     get "/", LinkController, :new
     get "/shorten/:id", LinkController, :show
     post "/shorten", LinkController, :create
-    get "/:shortcode", LinkController, :unshorten
   end
 end
