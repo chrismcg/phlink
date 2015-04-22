3c6482238d65de3f7d634761e8cf39c05954eb59
Chris McGrath <chris@chrismcg.com>
2015-04-22 14:08:58 +0200

    Try System.get_env

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -4,7 +4,7 @@ use Mix.Config
 # you likely want to automate and keep it away from
 # your version control system.
 config :phlink, Phlink.Endpoint,
-  secret_key_base: {:system, "SECRET_KEY_BASE"}
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
 config :phlink, Phlink.Repo,
