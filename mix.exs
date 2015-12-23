b7a45f601faac759738b5b1755743ca31b2f3897
Chris McGrath <chris@chrismcg.com>
2015-12-23 20:58:34 +0000

    Update ex_doc dependency

--- mix.exs
+++ mix.exs
@@ -57,7 +57,7 @@ defmodule Phlink.Mixfile do
      {:oauth2, "~> 0.3"},
      {:dotenv, "~> 2.0.0"},
      {:earmark, "~> 0.1", only: :dev},
-     {:ex_doc, "~> 0.10.0", only: :dev},
+     {:ex_doc, "~> 0.11.2", only: :dev},
      {:websocket_client, git: "https://github.com/jeremyong/websocket_client.git", only: :test},
      {:excoveralls, "~> 0.3.11", only: :test}
    ]
