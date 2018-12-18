c809b68a0bd086bf92df173aac67362c4500d4e7
Chris McGrath <chris@chrismcg.com>
2018-12-18 17:30:53 +0000

    Make the cookie configurable by env

--- rel/config.exs
+++ rel/config.exs
@@ -44,7 +44,7 @@ environment :prod do
 
   set include_erts: true
   set include_src: false
-  set cookie: :crypto.hash(:sha256, System.get_env("COOKIE") || "monster") |> Base.encode16() |> String.to_atom()
+  set cookie: :"THIS_IS_REPLACED_BY_ENV"
   set vm_args: "rel/vm.args"
 end
 
