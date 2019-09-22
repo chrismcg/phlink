ec2021186832e0508b4c43d6be041c2f60ce309e
Chris McGrath <chris@chrismcg.com>
2019-09-22 14:40:03 +0100

    More prod config tweaking

--- config/prod.exs
+++ config/prod.exs
@@ -1,10 +1,10 @@
 use Mix.Config
 
 config :phlink, PhlinkWeb.Endpoint,
-  http: [:inet6, port: 80],
+  http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
   url: [host: "phl.ink", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  secret_key_base: System.get_env("SECRET_KEY_BASE"),
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
   root: "."
 
 # Do not print debug messages in production
