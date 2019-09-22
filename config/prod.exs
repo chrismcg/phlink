a245250d0ac49e7c7033b833ce18a70bbb000ad8
Chris McGrath <chris@chrismcg.com>
2019-09-22 14:32:35 +0100

    Add Procfile and fix prod config

--- config/prod.exs
+++ config/prod.exs
@@ -1,8 +1,8 @@
 use Mix.Config
 
 config :phlink, PhlinkWeb.Endpoint,
-  http: [:inet6, port: String.to_integer(System.get_env("PORT"))],
-  url: [host: System.get_env("HOST") || "phl.ink", port: String.to_integer(System.get_env("URL_PORT") || "80")],
+  http: [:inet6, port: 80],
+  url: [host: "phl.ink", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
   root: "."
@@ -15,5 +15,6 @@ config :phoenix, serve_endpoints: true
 
 # Configure database via URL
 config :phlink, Phlink.Repo,
-  pool_size: 18,
-  url: System.get_env("DATABASE_URL")
+  url: System.get_env("DATABASE_URL"),
+  ssl: true,
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "18")
